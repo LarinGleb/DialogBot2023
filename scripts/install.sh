79d18e0ec8998915abbec0f8986018bc355e76d1
VforVindetta <gleblarinpo@gmail.com>
2023-07-01 00:09:39 +0300

    basic scripts

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+echo --------------------- INSTALL ---------------------
+
+echo ------- INSTALL DEP. FOR TGLIB ------- 
+
+sudo apt-get install g++ make binutils cmake libboost-system-dev libssl-dev zlib1g-dev libcurl4-openssl-dev
+
+echo ------- INSTALL TGLIB ------- 
+
+git clone https://github.com/reo7sp/tgbot-cpp
+cd tgbot-cpp
+cmake .
+make -j4
+sudo make install
+cd ..
+rm -rf tgbot-cpp
+
+
