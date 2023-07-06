38359504a6b6022713666c5aa3bd575292fb917c
VforVindetta <gleblarinpo@gmail.com>
2023-07-06 03:22:07 +0300

    All defines moved to bot.h

---
+++ bot/inc/bot.h
@@ -0,0 +1,11 @@
+#ifndef __BOT_H__
+#define __BOT_H__
+
+
+#define CURRENT_PATH std::filesystem::current_path().generic_string()
+#define MAIN_PATH CURRENT_PATH + "/../"
+
+#define TXT_PATH "files/"
+#define CREATION_CSV_PATH "temp/"
+
+#endif
