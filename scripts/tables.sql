24bc3fb90f2c43b32345c177491fc3f22f335f53
VforVindetta <gleblarinpo@gmail.com>
2023-07-06 03:23:44 +0300

    Sctruct of tabels in database

---
+++ scripts/tables.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE events (
+  ID serial PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  date DATE NOT NULL
+);
+
+CREATE TABLE reviews (
+  ID serial PRIMARY KEY NOT NULL,
+  event_id INT NOT NULL,
+  user_id INT NOT NULL,
+  date DATE NOT NULL,
+  mark_event INT NOT NULL,
+  change TEXT NOT NULL,
+  good TEXT NOT NULL,
+  next_year boolean NOT NULL
+);
